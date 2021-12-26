b47b655a2c53707de2f4552626fd02502987a01e
Tharisata Charoenvasnadumrong <tc511@cam.ac.uk>
2021-12-26 18:00:10 +0000

    Day 6 complete

--- code/day6.py
+++ code/day6.py
@@ -1,67 +1,53 @@
+import collections
 from util import read_input
 
-class LanternFish:
-    """ 
-    One Lanternfish object
-    """
-    def __init__(self, days: int):
-        self.days = days
-    
-    def _reset_timer(self) -> None:
-        self.days = 6
-    
-    def _countdown(self):
-        self.days -= 1
-    
-    def _create_new_lanternfish(self):
-        return LanternFish(days=8)
 
 class LanternFishGroup:
-    """ 
+    """
     A group of lanternfish
     """
+
     def __init__(self):
-        self.fish: list[LanternFish] = []
-    
-    def add_fish(self, fish: LanternFish):
-        self.fish.append(fish)
-    
+        """
+        The amount of fish per day
+        """
+        days = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        self.days = {key: value for key, value in zip(days, [0] * len(days))}
+
+    def add_fish(self, day: int):
+        self.days[day] += 1
+
     def countdown(self, countdown_days: int):
-        """ 
+        """
         Reset fish with 0 days to 6 and creates a new lantern fish.
         """
         for _ in range(countdown_days):
-            new_fish = []
-            for fish in self.fish:
-                if fish.days != 0:
-                    fish._countdown()
-                else:
-                    fish._reset_timer()
-                    new_fish.append(fish._create_new_lanternfish())
+            day0 = self.days[0]
+            for day in [0, 1, 2, 3, 4, 5, 6, 7]:
+                self.days[day] = self.days[day + 1]
+            self.days[6] += day0
+            self.days[8] = day0
 
-            self.fish.extend(new_fish)
-    
-    def print_days(self):
-        print("Number of fish:", len(self.fish))
-        # for fish in self.fish:
-        #     print(fish.days)
+    def print_num_fish(self):
+        print("Number of fish:", sum(val for val in self.days.values()))
 
 
 def get_days(line: str):
-    """ 
+    """
     Split line into days
     """
-    return map(int,line.split(","))
+    return map(int, line.split(","))
+
 
 def main():
     fish_group = LanternFishGroup()
     all_days = get_days(read_input("day6.txt")[0])
     for days in all_days:
-        fish_group.add_fish(LanternFish(days))
+        fish_group.add_fish(days)
 
-    fish_group.countdown(80)
-    fish_group.print_days()
-    
-    
-if __name__ == '__main__':
+    fish_group.countdown(256)
+    fish_group.print_num_fish()
+
+
+if __name__ == "__main__":
     main()
